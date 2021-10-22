cf1a3e0ea65b2b25ffab0c3384b1e8d40fee65ed
PedroLuisBrilhadori <pedroluisbrilhadori@gmail.com>
2021-10-21 22:58:28 -0300

    fix: adiciona clientes CRUD

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ClientesModule } from './clientes/clientes.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [],
+  imports: [
+    MongooseModule.forRoot('mongodb+srv://pedrin:p3dr1nh0lb@cluster0.e0amz.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'),
+    ClientesModule, 
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
